8c61f55e99f56d3bae898e622601369c53d682c0
mgmilton <matt.milton@gmail.com>
2018-05-13 10:14:30 -0600

    sets up presentation of food

--- src/main/java/quantified_self/resource/FoodResource.java
+++ src/main/java/quantified_self/resource/FoodResource.java
@@ -4,3 +4,31 @@ import org.springframework.hateoas.ResourceSupport;
 
 import quantified_self.domain.Food;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+public class FoodResource extends ResourceSupport {
+
+      private final long id;
+      private final String name;
+      private final long calories;
+
+
+      public FoodResource(Food food) {
+            id = food.getId();
+            name = food.getName();
+            calories = food.getCalories();
+      }
+
+      @JsonProperty("id")
+      public Long getResourceId() {
+            return id;
+      }
+
+      public String getName() {
+            return name;
+      }
+
+      public long getCalories() {
+            return calories;
+      }
+}
