6351e2163a3dfeecab2265bada6c329c562db9fe
mgmilton <matt.milton@gmail.com>
2018-05-13 13:31:31 -0600

    updates links on resources

--- src/main/java/quantified_self/resource/FoodResourceAssembler.java
+++ src/main/java/quantified_self/resource/FoodResourceAssembler.java
@@ -11,22 +11,22 @@ import quantified_self.domain.Food;
 @Component
 public class FoodResourceAssembler extends ResourceAssembler<Food, FoodResource> {
 
-	@Autowired
-	protected EntityLinks entityLinks;
-
-	private static final String UPDATE_REL = "update";
-	private static final String DELETE_REL = "delete";
+	// @Autowired
+	// protected EntityLinks entityLinks;
+	//
+	// private static final String UPDATE_REL = "update";
+	// private static final String DELETE_REL = "delete";
 
 	@Override
 	public FoodResource toResource(Food food) {
 
 		FoodResource resource = new FoodResource(food);
 
-		final Link selfLink = entityLinks.linkToSingleResource(food);
-
-		resource.add(selfLink.withSelfRel());
-		resource.add(selfLink.withRel(UPDATE_REL));
-		resource.add(selfLink.withRel(DELETE_REL));
+		// final Link selfLink = entityLinks.linkToSingleResource(food);
+		//
+		// resource.add(selfLink.withSelfRel());
+		// resource.add(selfLink.withRel(UPDATE_REL));
+		// resource.add(selfLink.withRel(DELETE_REL));
 
 		return resource;
 	}
--- src/main/java/quantified_self/resource/MealResourceAssembler.java
+++ src/main/java/quantified_self/resource/MealResourceAssembler.java
@@ -11,22 +11,22 @@ import quantified_self.domain.Meal;
 @Component
 public class MealResourceAssembler extends ResourceAssembler<Meal, MealResource> {
 
-	@Autowired
-	protected EntityLinks entityLinks;
-
-	private static final String UPDATE_REL = "update";
-	private static final String DELETE_REL = "delete";
+	// @Autowired
+	// protected EntityLinks entityLinks;
+	//
+	// private static final String UPDATE_REL = "update";
+	// private static final String DELETE_REL = "delete";
 
 	@Override
 	public MealResource toResource(Meal meal) {
 
 		MealResource resource = new MealResource(meal);
 
-		final Link selfLink = entityLinks.linkToSingleResource(meal);
-
-		resource.add(selfLink.withSelfRel());
-		resource.add(selfLink.withRel(UPDATE_REL));
-		resource.add(selfLink.withRel(DELETE_REL));
+		// final Link selfLink = entityLinks.linkToSingleResource(meal);
+		//
+		// resource.add(selfLink.withSelfRel());
+		// resource.add(selfLink.withRel(UPDATE_REL));
+		// resource.add(selfLink.withRel(DELETE_REL));
 
 		return resource;
 	}
