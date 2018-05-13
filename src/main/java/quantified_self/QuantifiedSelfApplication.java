6162131aaa33ea87163125e5aed5f8b31bffb4d7
mgmilton <matt.milton@gmail.com>
2018-05-13 09:53:03 -0600

    renames application

--- src/main/java/quantified_self/QuantifiedSelfApplication.java
+++ src/main/java/quantified_self/QuantifiedSelfApplication.java
@@ -1,12 +1,12 @@
-package com.example.demo;
+package quantified_self;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class QuantifiedSelfApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(QuantifiedSelfApplication.class, args);
 	}
 }
